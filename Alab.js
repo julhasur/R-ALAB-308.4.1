991872b7ecb69c5fac46e90d7204e0b1cf933eed
Julhasur Chowdhury <37052892+julhasur@users.noreply.github.com>
2024-11-17 22:53:40 -0500

    fizz buzz complete

--- Alab.js
+++ Alab.js
@@ -1,11 +1,18 @@
 //part 1:Fizz Buzz
 
-for(let i=1;i<100;i++){
+for(let i=1;i<=100;i++){
   if(i % 3 ==0){
     console.log(i)
     console.log("Fizz");
   }
   else if(i % 5==0){
+    console.log(i)
     console.log("Buzz");
+  }
+  if(i % 3==0 && i % 5==0){
+    console.log("fizz Buzz");
+  }
+  else if (i % 3 !==0 || i % 5 !==0){
+    console.log((i)+" not divisible buy 5 or 3 ")
   }
 }
