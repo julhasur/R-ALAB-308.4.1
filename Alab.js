10df5b71b0698d32bb86ebac4cde81be27d3f2e7
Julhasur Chowdhury <37052892+julhasur@users.noreply.github.com>
2024-11-18 16:18:30 -0500

    part2 done

--- Alab.js
+++ Alab.js
@@ -3,17 +3,17 @@
 
 
 //part 1:Fizz Buzz
-// for (let i = 1; i <= 100; i++) {
-//   if (i % 3 === 0 && i % 5 === 0) {
-//     console.log("Fizz Buzz");
-//   } else if (i % 3 === 0) {
-//     console.log("Fizz");
-//   } else if (i % 5 === 0) {
-//     console.log("Buzz");
-//   } else {
-//     console.log(i + " not divisible by 5 or 3");
-//   }
-// }
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("Fizz Buzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i + " not divisible by 5 or 3");
+  }
+}
 //part 2 Prime time
 // instead of declating an numbner am just making a function so it cn be tested with any number in param;
 // creating the function to check if input numberis prime to begin with
@@ -39,14 +39,14 @@ function isPrime(num){
 //  }
 //  }
 
-// function findnextprime(n){
-//   let number=n+1;
-//   while (!isPrime(number)){
-//  number++;
-//   }
-//   return number;
-// }
-// console.log(findnextprime(14));
+function findnextprime(n){
+  let number=n+1;
+  while (!isPrime(number)){
+ number++;
+  }
+  return number;
+}
+console.log(findnextprime(14));
 //part 3
  function findrow(str){
   let rows=[];
@@ -64,6 +64,7 @@ function isPrime(num){
  }
 
 let finalrows=findrow("Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232");
+
 
 function findcell(rows){
   for(let i=0;i<rows.length;i++){
@@ -84,4 +85,49 @@ function findcell(rows){
     console.log(currentcell);
   }
 }
-findcell(finalrows);
+// findcell(finalrows);
+
+
+
+//Part 2
+//Expading functionality
+function findDynamicRow(str){
+  let rows=[];
+  let currentrow="";
+  for(let i=0;i<str.length;i++){
+    if(str[i]==="\n"){
+      rows.push(currentrow)
+      currentrow=""
+    }
+    else(
+      currentrow+=str[i]
+    )
+  }
+  return rows
+ }
+ let dynamicrow=findDynamicRow("ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26");
+ console.log(dynamicrow);
+
+ function twodArray(rows){
+  let twodArray=[]
+  for(let i=0;i<rows.length;i++){
+    let rowarray=[];
+    let currentrow="";
+    
+    for(let j=0;j<rows[i].length;j++){
+      if(rows[i][j]===","){
+        rowarray.push(currentrow);
+        currentrow="";
+      }
+      else(
+        currentrow+=rows[i][j]
+      )
+    }
+    rowarray.push(currentrow);
+    twodArray.push(rowarray)
+  }
+  return twodArray;
+}
+let csvarray= twodArray(dynamicrow);
+console.log("formated csv:",csvarray);
+console.log("number of columns:",csvarray[0].length);
